a5e16b56c94466783850a62ef2d47be232a583c3
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-05-21 00:12:24 +0900

    test_rotation.py completed

--- arrays_strings/rotation/test_rotation.py
+++ arrays_strings/rotation/test_rotation.py
@@ -1,5 +1,28 @@
 import unittest
+""" rotation_challenge.ipynbに記載のURLのAlgorithmを参考にする。
+元の文字列をずらしているのでs1 in s2+s2でrotationしたかどうかが判明する。
 
+rotation_challenge.ipynbではclass Rotation(object)が定義されており、かつ、
+Problem: Determine if a string s1 is a rotation of another string s2, by calling (only once) a function is_substring.の記述があるので
+コピーの上利用する。
+"""
+class Rotation(object):
+
+    def is_substring(self, s1, s2):
+        # TODO: Implement me
+        # pass
+        return (s1 in s2+s2)
+
+    def is_rotation(self, s1, s2):
+        # TODO: Implement me
+        # Call is_substring only once
+        # pass
+        if s1 == None or s2 == None:    return False
+        if len(s1) != len(s2):          return False
+        if len(s1) == 0 and len(s2) == 0:   return True
+        if len(s1) == 0 and len(s2) > 0:    return False
+        if len(s1) > 0 and len(s2) == 0:    return False
+        return (self.is_substring(s1,s2))
 
 class TestRotation(unittest.TestCase):
 
@@ -7,13 +30,16 @@ class TestRotation(unittest.TestCase):
         rotation = Rotation()
         self.assertEqual(rotation.is_rotation('o', 'oo'), False)
         self.assertEqual(rotation.is_rotation(None, 'foo'), False)
+        self.assertEqual(rotation.is_rotation('foo', None), False) # added
         self.assertEqual(rotation.is_rotation('', 'foo'), False)
+        self.assertEqual(rotation.is_rotation('foo', ''), False) # added
         self.assertEqual(rotation.is_rotation('', ''), True)
         self.assertEqual(rotation.is_rotation('foobarbaz', 'barbazfoo'), True)
         print('Success: test_rotation')
 
-
+    
 def main():
+    pass
     test = TestRotation()
     test.test_rotation()
 
