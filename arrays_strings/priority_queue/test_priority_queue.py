1b4038e439ba615070269bb2c3b312e2a396859a
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-05-21 23:03:05 +0900

    test_priority_queue.py completed

--- arrays_strings/priority_queue/test_priority_queue.py
+++ arrays_strings/priority_queue/test_priority_queue.py
@@ -1,6 +1,67 @@
+""" priority_queue.pyは削除する
+その上でclass PriorityQueueNode(object):とclass PriorityQueue(object):をpriority_queue_challenge.ipynbからコピーする
+
+前提：Can we assume there aren't any duplicate keys?  Yes
+
+extract_min()について
+    while priority_queue.array:
+        mins.append(priority_queue.extract_min().key)
+
+    priority_queue.arrayがなくならないとループを抜けない
+    extract_min()が一度呼ばれると、
+        その時点で一番小さいkeyを探す
+        ループを抜けるため必ずpop()する
+
+"""
 import unittest
+import sys
+
+class PriorityQueueNode(object):
+
+    def __init__(self, obj, key):
+        self.obj = obj
+        self.key = key
+
+    def __repr__(self):
+        return str(self.obj) + ': ' + str(self.key)
 
 
+class PriorityQueue(object):
+
+    def __init__(self):
+        self.array = []
+
+    def __len__(self):
+        return len(self.array)
+
+    def insert(self, node):
+        # TODO: Implement me
+        # pass
+        self.array.append(node)
+
+    def extract_min(self):
+        # TODO: Implement me
+        # pass
+        if len(self.array) == 0: return None
+        min_val = sys.maxsize
+        min_pos = -1
+        for i, n in enumerate(self.array):
+            if min_val > (current_val := n.key):
+                min_val = current_val
+                min_pos = i
+        return self.array.pop(min_pos)
+
+
+    def decrease_key(self, obj, new_key):
+        # TODO: Implement me
+        # pass
+        for n in self.array:
+            if n.obj == obj:
+                n.key = new_key
+
+    def sort_queue(self):
+        return    sorted([self.array[i].key  for i in range(len(self.array))])
+        
 class TestPriorityQueue(unittest.TestCase):
 
     def test_priority_queue(self):
@@ -14,10 +75,12 @@ class TestPriorityQueue(unittest.TestCase):
         priority_queue.insert(PriorityQueueNode('f', 3))
         priority_queue.decrease_key('f', 2)
         priority_queue.decrease_key('a', 19)
-        mins = []
-        while priority_queue.array:
-            mins.append(priority_queue.extract_min().key)
-        self.assertEqual(mins, [2, 5, 15, 19, 22, 40])
+        print("=== ", priority_queue.array)
+        self.assertEqual(priority_queue.sort_queue(),  [2, 5, 15, 19, 22, 40])
+        # mins = [] 
+        # while priority_queue.array:
+        #     mins.append(priority_queue.extract_min().key)
+        # self.assertEqual(mins, [2, 5, 15, 19, 22, 40])
         print('Success: test_min_heap')
 
 
