54090826f75441cb007227a0f14cc0a9f79dc264
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-05-21 15:42:33 +0900

    test_reverse_string.py completed

--- arrays_strings/reverse_string/test_reverse_string.py
+++ arrays_strings/reverse_string/test_reverse_string.py
@@ -1,5 +1,19 @@
 import unittest
 
+""" 以下のサイトを参考にする。
+https://ja.wikipedia.org/wiki/In-place%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
+"""
+
+class ReverseString(object):
+    def reverse(self,chars):
+        # pass
+        if chars == None: return None
+        if chars == ['']: return ['']
+
+        for i in range(len(chars)//2):
+            chars[i], chars[len(chars)-i-1] = chars[len(chars)-i-1], chars[i]
+        
+        return chars    
 
 class TestReverse(unittest.TestCase):
 
