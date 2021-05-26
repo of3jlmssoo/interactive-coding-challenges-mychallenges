9399bf22c9137ffb6afb68fc8616001c2cf0792f
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-05-26 22:18:30 +0900

    test_fizz_buzz.py completed

--- arrays_strings/fizz_buzz/test_fizz_buzz.py
+++ arrays_strings/fizz_buzz/test_fizz_buzz.py
@@ -1,12 +1,29 @@
 import unittest
 
 
+class Solution(object):
+
+    def fizz_buzz(self, num):
+        # TODO: Implement me
+        # pass
+        if num == None: raise TypeError(f'num is None')        
+        if num == 0: raise ValueError(f'0 is specified(valueError)')
+
+        result = []
+        for i in range(1,num+1):
+            result.append(str(i))
+            if i%3 == 0: result[i-1] = 'Fizz'
+            if i%5 == 0: result[i-1] = 'Buzz'
+            if i%3 == 0 and i%5 == 0: result [i-1]= 'FizzBuzz'
+        return result
+
 class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz(self):
         solution = Solution()
         self.assertRaises(TypeError, solution.fizz_buzz, None)
         self.assertRaises(ValueError, solution.fizz_buzz, 0)
+
         expected = [
             '1',
             '2',
