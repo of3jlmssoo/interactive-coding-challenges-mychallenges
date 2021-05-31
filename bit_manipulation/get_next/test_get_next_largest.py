9cda023cd1e85cf44cc5b989d0d078cdbf18d090
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-05-31 15:49:53 +0900

    test_get_next_large.py check_num() and change_num() are added for refactoring

--- bit_manipulation/get_next/test_get_next_largest.py
+++ bit_manipulation/get_next/test_get_next_largest.py
@@ -2,32 +2,54 @@ import unittest
 
 
 class Bits(object):
-    
-    def get_next_largest(self, num: int) -> int:
-        if num in [None,0,-1]: raise(Exception)
 
-        num_bin_splitted_01 = bin(num)[2:].split('01') # remove first two charcter '0b' and split by '01'
-        num_bin_splitted_01_len = len(num_bin_splitted_01)
-        result = ''
+    def check_num(self, num:int ) -> bool:
+        if num in [None,0,-1]: raise(Exception)
+        else: return True
+
+    def get_next_largest(self, num: int) -> int:
+
+        self.check_num(num) 
+
         append_candidate = ['01','10']
-        for i in range(num_bin_splitted_01_len-1): # -1 : as for the last element in num_bin_splitted_01
-                                                   # it will be added to result without any append_char in return statement
-            append_char = append_candidate[(num_bin_splitted_01_len-2)==i]
-                                                   # -2 : the last '01' will be replace with '10'
-            result = result + num_bin_splitted_01[i] + append_char
-        return int(result + num_bin_splitted_01[num_bin_splitted_01_len-1],base=2)
+        # num_bin_splitted_01 = bin(num)[2:].split(append_candidate[0]) # remove first two charcter '0b' and split by '01'
+        # num_bin_splitted_01_len = len(num_bin_splitted_01)
+        # result = ''
+        # for i in range(num_bin_splitted_01_len-1): # -1 : as for the last element in num_bin_splitted_01
+        #                                            # it will be added to result without any append_char in return statement
+        #     append_char = append_candidate[(num_bin_splitted_01_len-2)==i]
+        #                                            # -2 : the last '01' will be replace with '10'
+        #     result = result + num_bin_splitted_01[i] + append_char
+        # return int(result + num_bin_splitted_01[num_bin_splitted_01_len-1],base=2)
+        return self.change_num(append_candidate, num) 
 
     def get_next_smallest(self, num: int) -> int:
-        if num in [None,0,-1]: raise(Exception)
+        # if num in [None,0,-1]: raise(Exception)
+        self.check_num(num) 
 
-        num_bin_splitted_01 = bin(num)[2:].split('10') # remove first two charcter '0b' and split by '01'
-        num_bin_splitted_01_len = len(num_bin_splitted_01)
-        result = ''
         append_candidate = ['10','01']
+        # num_bin_splitted_01 = bin(num)[2:].split(append_candidate[0]) # remove first two charcter '0b' and split by '01'
+        # num_bin_splitted_01_len = len(num_bin_splitted_01)
+        # result = ''
+        # for i in range(num_bin_splitted_01_len-1): # -1 : as for the last element in num_bin_splitted_01
+        #                                            # it will be added to result without any append_char in return statement
+        #     append_char = append_candidate[(num_bin_splitted_01_len-2)==i]
+        #                                            # -2 : the last '01' will be replace with '10'
+        #     result = result + num_bin_splitted_01[i] + append_char
+        
+        return self.change_num(append_candidate, num) 
+            # print("same!")
+
+        # return int(result + num_bin_splitted_01[num_bin_splitted_01_len-1],base=2)
+
+    def change_num(self, candidates: list, num:int) -> int:
+        result = ''
+        num_bin_splitted_01 = bin(num)[2:].split(candidates[0]) # remove first two charcter '0b' and split by '01' or '10' (cadidate[0])
+        num_bin_splitted_01_len = len(num_bin_splitted_01)
         for i in range(num_bin_splitted_01_len-1): # -1 : as for the last element in num_bin_splitted_01
                                                    # it will be added to result without any append_char in return statement
-            append_char = append_candidate[(num_bin_splitted_01_len-2)==i]
-                                                   # -2 : the last '01' will be replace with '10'
+            append_char = candidates[(num_bin_splitted_01_len-2)==i]
+                                                   # -2 : the last candidates[0] will be replace with candidates[1]
             result = result + num_bin_splitted_01[i] + append_char
         return int(result + num_bin_splitted_01[num_bin_splitted_01_len-1],base=2)    
 
