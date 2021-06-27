c69299ab550a9296873809b28052181c9dd7c41d
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-06-27 23:56:58 +0900

    test_path_exists.py completed

---
+++ graphs_trees/graph_path_exists/results.py
@@ -0,0 +1,1 @@
+../utils/results.py
--- graphs_trees/graph_path_exists/test_path_exists.py
+++ graphs_trees/graph_path_exists/test_path_exists.py
@@ -1,5 +1,68 @@
-import unittest
+""" test_path_exists.py
 
+ln -s  ../utils/results.py
+ln -s  ../graph/test_graph.py
+
+"""
+import unittest
+import logging
+from test_graph import State
+from test_graph import Node
+from test_graph import Graph
+from test_graph import State
+from results    import Results
+from collections import deque
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = True
+
+class ResultsNode(Results):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def add_result(self, result):
+        self.results.append(result)
+
+class GraphPathExists(Graph):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.results = ResultsNode()
+
+    def add_node(self,id):
+        super().add_node(id)
+        return self.nodes[id]
+
+    def path_exists(self, start, end):
+        self.results.clear_results()
+        return self.path_exists_body(start, end)
+
+    def path_exists_body(self, start, end):
+        # TODO: Implement me
+        # pass
+        logger.debug(f'GraphPathExists.path_exists() start:{start}, start.visit_state:{start.visit_state}')
+        start.visit_state = State.visited
+        # visit_func(start)
+        self.results.add_result(start)
+        # logger.debug(f'start.self.adj_nodes.values(): {start.adj_nodes.values()}') 
+        for r in start.adj_nodes.values():
+            if r.visit_state == State.unvisited:
+                self.path_exists_body(r, self.results.add_result)
+        
+        logger.debug(f'GraphPathExists.path_exists() end:{end} type(end):{type(end)} in start.adj_nodes:{start.adj_nodes}. self.results:{self.results} type(self.results):{type(self.results)}')
+        if end in self.results.results:
+            return True
+        else:
+            return False
 
 class TestPathExists(unittest.TestCase):
 
@@ -17,6 +80,7 @@ class TestPathExists(unittest.TestCase):
         graph.add_edge(3, 2, 7)
         graph.add_edge(3, 4, 8)
 
+        # logger.debug(f'TestPathExists.test_path_exists() nodes:{nodes}')
         self.assertEqual(graph.path_exists(nodes[0], nodes[2]), True)
         self.assertEqual(graph.path_exists(nodes[0], nodes[0]), True)
         self.assertEqual(graph.path_exists(nodes[4], nodes[5]), False)
