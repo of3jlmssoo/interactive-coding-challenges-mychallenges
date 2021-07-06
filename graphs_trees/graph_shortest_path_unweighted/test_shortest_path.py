495b0783815f1fa08b26f3a65c0cc4804db68347
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-06 22:58:43 +0900

    test_shortest_path(unweighted).py completed

--- graphs_trees/graph_shortest_path_unweighted/test_shortest_path.py
+++ graphs_trees/graph_shortest_path_unweighted/test_shortest_path.py
@@ -18,9 +18,9 @@ logger.disabled = False
 
 class GraphShortestPath(Graph):
 
-    # def __init__(self):
-    #     super().__init__()
-    #     self.__graph = 
+    def __init__(self):
+        super().__init__()
+        self.__shortest_path = None 
 
     def add_node(self,id):
         id=str(id)
@@ -37,9 +37,30 @@ class GraphShortestPath(Graph):
         pass
 
         # print(self.nodes)
-        shortest_path = ShortestPath(self)
-        result = shortest_path.find_shortest_path(str(source_key), str(dest_key))
-        print(result)
+
+        """
+        HACK:ShortestPath(self)のコールがここでこの形態で良いか検討する。
+        以下の理由からここに、この形態に至る。
+        - ShortestPath()を利用したい 
+        - GraphShortestPath()の__init__でコールするとエラーになる
+        - shortest_path()は同じグラフを前提に繰返しコールされる
+        """
+        if self.__shortest_path == None:
+            self.__shortest_path = ShortestPath(self)
+        
+        result = self.__shortest_path.find_shortest_path(str(source_key), str(dest_key))
+        
+
+        """ルートが無い場合self.__shortest_path.find_shortest_path()がNoneを返すことに対応"""
+        """
+        HACK:resultに要素がある場合int()で数値に変換できることを前提にしている。
+        アルファベットの場合ValueErrorが起きるのでこれをキャッチするように修正する
+        """
+        if result != None:
+            for i in range(len(result)):
+                result[i] = int(result[i])
+
+        return result
 
 class TestShortestPath(unittest.TestCase):
 
@@ -57,12 +78,12 @@ class TestShortestPath(unittest.TestCase):
         graph.add_edge(3, 2)
         graph.add_edge(3, 4)
 
-        graph.return_nodes()
-        print("-----", graph.shortest_path(nodes[0].key, nodes[2].key) )
+        # graph.return_nodes()
+        # print("-----", graph.shortest_path(nodes[0].key, nodes[2].key) )
 
-        # self.assertEqual(graph.shortest_path(nodes[0].key, nodes[2].key), [0, 1, 3, 2])
-        # self.assertEqual(graph.shortest_path(nodes[0].key, nodes[0].key), [0])
-        # self.assertEqual(graph.shortest_path(nodes[4].key, nodes[5].key), None)
+        self.assertEqual(graph.shortest_path(nodes[0].key, nodes[2].key), [0, 1, 3, 2])
+        self.assertEqual(graph.shortest_path(nodes[0].key, nodes[0].key), [0])
+        self.assertEqual(graph.shortest_path(nodes[4].key, nodes[5].key), None)
 
         print('Success: test_shortest_path')
 
