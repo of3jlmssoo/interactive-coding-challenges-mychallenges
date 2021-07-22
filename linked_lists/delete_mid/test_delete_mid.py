766379ac7363be09964fc74af70234448aea475c
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-22 23:26:40 +0900

    test_delete_mid.py completed

--- linked_lists/delete_mid/test_delete_mid.py
+++ linked_lists/delete_mid/test_delete_mid.py
@@ -1,4 +1,126 @@
+"""
+[課題]
+今ひとつ理解できず。
+Test Casesは以下の記述。
+Delete on empty list -> None
+Delete None -> None
+Delete on one node -> [None]
+Delete on multiple nodes
+
+実際のテストは以下の4つ。Test Casesと上手く関連付けできず。
+1)テスト1
+MyLinkedList(None)
+delete_node(None)
+(しかし)get_all_data()で[]を返す。
+ということは、MyLinkedList(None)でself.q=[None]とする。
+test_linked_listのLinkedListではself.qにNoneは含めないようになっている
+この点の修正が必要
+
+2) テスト2
+Noneではない唯一のエントリーを削除した場合self.q=[None]とする
+self.headの修正も必要
+
+3) テスト3
+最初でのもなく最後でもないエントリーを削除した場合Noneは登場せず
+削除したエントリーの前後をつなげる
+
+4) テスト4
+最後のエントリーを削除した場合最後のエントリーをNoneにする。
+直前のエントリーのnode.linkをNoneに設定する
+
+"""
 import unittest
+import logging
+from test_linked_list import Node, LinkedList
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = True
+
+
+class MyLinkedList(LinkedList):
+
+    def __init__(self, head):
+        super().__init__(head=head)
+        # Noneをnodeとして追加する。self.qが空の時のみNoneをノードとしてセットする
+        if not self.q and head is None:
+            self.q = [None]
+
+    def insert_to_front(self, data):
+        logger.debug(f'MyLinkedList.insert_to_front. called {data} {self.q}')
+        if len(self.q) == 1 and self.q[0] is None:
+            self.q = []
+        super().insert_to_front(data)
+        return self.node
+
+    def delete_node(self, node):
+        logger.debug(f'MyLinkedList.delete_node 1. called {self.q}')
+        # 1)テスト1
+        # MyLinkedList(None)
+        # delete_node(None)
+        # (しかし)get_all_data()で[]を返す。
+        # ということは、MyLinkedList(None)でself.q=[None]とする。
+        # test_linked_listのLinkedListではself.qにNoneは含めないようになっている
+        # この点の修正が必要
+        # 2) テスト2
+        # Noneではない唯一のエントリーを削除した場合self.q=[None]とする
+        # self.headの修正も必要
+        if len(self.q) == 1:
+            if node is None:
+                self.q = []
+            else:
+                self.q[0] = None
+            return
+        # 3) テスト3
+        # 最初でのもなく最後でもないエントリーを削除した場合Noneは登場せず
+        # 削除したエントリーの前後をつなげる
+        #  0 1 2 3 4
+        # [1,2,3,4,5]
+        #      A
+        logger.debug(
+            f'MyLinkedList.delete_node 3-1. called {node} -- and -- {self.q}')
+        logger.debug(
+            f'MyLinkedList.delete_node 3-2. called {self.q.index(node)}  {len(self.q) - 1}')
+        idx = self.q.index(node)
+        if 1 < idx < (len(self.q) - 1):
+            self.q = list(filter(lambda x: x != node, self.q))
+            self.q[idx - 1].link = self.q[idx]
+            if not logger.disabled:
+                for n in self.q:
+                    print(
+                        f'MyLinkedList.delete_node 3-3.{n.data} and {n.link}')
+            # 4) テスト4
+            # 最後のエントリーを削除した場合最後のエントリーをNoneにする。
+            # 直前のエントリーのnode.linkをNoneに設定する
+        elif idx == len(self.q) - 1:
+            self.q[-1] = None
+            self.q[-2].link = self.q[-1]
+
+        if not logger.disabled:
+            for n in self.q:
+                if n is not None:
+                    print(
+                        f'MyLinkedList.delete_node 3-4.{n.data} and {n.link}')
+                else:
+                    print(
+                        f'MyLinkedList.delete_node 3-5.{n}')
+        logger.debug(f'MyLinkedList.delete_node. end {self.q}')
+
+    def ls_nodes(self):
+        print(f'MyLinkedList.ls_nodes. {self.q}')
+
+    # def get_all_data(self):
+    #     # pass
+    #     # TODO: Implement me
+    #     return [n.data if n is not None else None for n in self.q]
 
 
 class TestDeleteNode(unittest.TestCase):
