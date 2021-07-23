5564adaacd4cba5bad70a04143330637bc23184e
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-23 20:52:11 +0900

    test_remove_duplicates.py completed

--- linked_lists/remove_duplicates/test_remove_duplicates.py
+++ linked_lists/remove_duplicates/test_remove_duplicates.py
@@ -1,5 +1,6 @@
 import unittest
 import logging
+
 from test_linked_list import LinkedList, Node
 
 logger = logging.getLogger(__name__)
@@ -21,12 +22,30 @@ class MyLinkedList(LinkedList):
         # TODO: Implement me
         pass
 
-        # Empty linked list -> []
+        """ Empty linked list -> [] """
         if not self.q:
             return self.q
-        # One element linked list -> [element]
-        # General case with no duplicates
-        # General case with duplicates
+        """ One element linked list -> [element] """
+        if len(self.q) == 1:
+            return self.q[0]
+        """ General case with no duplicates """
+        """ General case with duplicates """
+        """ self.qからnodeをno_dupes_nodesへ、dataをno_dupes_dataへ移す。
+            ただしdataが重複しないものだけを移す。  """
+        no_dupes_nodes = []
+        no_dupes_data = []
+        for n in self.q:
+            if n.data not in no_dupes_data:
+                no_dupes_data.append(n.data)
+                no_dupes_nodes.append(n)
+        self.q = no_dupes_nodes
+
+        """ node.linkを正しくセットする"""
+        for n in range(len(self.q) - 1):
+            self.q[n].link = self.q[n + 1]
+        self.q[-1].link = None
+        # for n in self.q:
+        #     print(f'--{n.data} {n.link}')
 
 
 class TestRemoveDupes(unittest.TestCase):
@@ -42,19 +61,19 @@ class TestRemoveDupes(unittest.TestCase):
         self.assertEqual(linked_list.get_all_data(), [2])
 
         print('Test: General case, duplicates')
-        # linked_list.insert_to_front(1)
-        # linked_list.insert_to_front(1)
-        # linked_list.insert_to_front(3)
-        # linked_list.insert_to_front(2)
-        # linked_list.insert_to_front(3)
-        # linked_list.insert_to_front(1)
-        # linked_list.insert_to_front(1)
-        # linked_list.remove_dupes()
-        # self.assertEqual(linked_list.get_all_data(), [1, 3, 2])
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(3)
+        linked_list.insert_to_front(2)
+        linked_list.insert_to_front(3)
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(1)
+        linked_list.remove_dupes()
+        self.assertEqual(linked_list.get_all_data(), [1, 3, 2])
 
-        # print('Test: General case, no duplicates')
-        # linked_list.remove_dupes()
-        # self.assertEqual(linked_list.get_all_data(), [1, 3, 2])
+        print('Test: General case, no duplicates')
+        linked_list.remove_dupes()
+        self.assertEqual(linked_list.get_all_data(), [1, 3, 2])
 
         print('Success: test_remove_dupes\n')
 
