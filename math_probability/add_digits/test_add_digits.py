094a77bdceb8e168161c42b117bd88dbaca146da
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-24 22:53:10 +0900

    test_add_digits.py completed

--- math_probability/add_digits/test_add_digits.py
+++ math_probability/add_digits/test_add_digits.py
@@ -1,4 +1,53 @@
 import unittest
+import logging
+
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = False
+
+
+class Solution(object):
+
+    def add_digits(self, val):
+        # TODO: Implement me
+        pass
+
+        logger.debug(f'Solution.add_digits: called. {val}')
+        """ None input -> TypeError """
+        if val is None:
+            raise TypeError(f'Solution.add_digits: val == None')
+        """ negative input -> ValueError """
+        if val < 0:
+            raise ValueError(f'Solution.add_digits: val < 0')
+        """ 9 -> 9 """
+        if val < 10:
+            return val
+        """ 138 -> 3 """
+        """ 65536 -> 7 """
+        while self.isTwoDigits(val):
+            val = self.transform(val)
+        return val
+
+    def isTwoDigits(self, val):
+        logger.debug(f'Solution.isTwoDigits: {val}')
+        return len(list(str(val))) > 1
+
+    def transform(self, val):
+
+        result = 0
+        for idx in reversed(list(str(val))):
+            result = result + int(idx)
+        logger.debug(f'Solution.transform: {result}')
+        return result
 
 
 class TestAddDigits(unittest.TestCase):
@@ -9,7 +58,7 @@ class TestAddDigits(unittest.TestCase):
         self.assertEqual(func(0), 0)
         self.assertEqual(func(9), 9)
         self.assertEqual(func(138), 3)
-        self.assertEqual(func(65536), 7) 
+        self.assertEqual(func(65536), 7)
         print('Success: test_add_digits')
 
 
@@ -19,7 +68,7 @@ def main():
     test.test_add_digits(solution.add_digits)
     try:
         test.test_add_digits(solution.add_digits_optimized)
-    except NameError:
+    except AttributeError:
         # Alternate solutions are only defined
         # in the solutions file
         pass
