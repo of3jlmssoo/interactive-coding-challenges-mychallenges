a0f7982af4a96304bd34c30f9612d41ff0e4a19b
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-24 23:06:13 +0900

    test_sum_two.py completed

--- math_probability/sum_two/test_sum_two.py
+++ math_probability/sum_two/test_sum_two.py
@@ -1,4 +1,35 @@
 import unittest
+import logging
+
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = False
+
+
+class Solution(object):
+    """
+    ちょっと違う気もするし、sub_twoどうしようはあるが+も-も使っていないことに間違い無し。。。
+    """
+
+    def sum_two(self, val1, val2):
+        # TODO: Implement me
+        # pass
+        logger.debug(f'Solution.sum_two: {val1} {val2}')
+        if val1 is None or val2 is None:
+            raise TypeError(f'Solution.sum_two: val == None')
+        lst = []
+        lst.append(val1)
+        lst.append(val2)
+        return sum(lst)
 
 
 class TestSumTwo(unittest.TestCase):
