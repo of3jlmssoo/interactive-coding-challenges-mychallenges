6bcbca784e0a04476ff353e2738c14bc9e8c7486
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-24 19:16:21 +0900

    test_partition.py completed

--- linked_lists/partition/test_partition.py
+++ linked_lists/partition/test_partition.py
@@ -1,4 +1,68 @@
+"""
+リストを3つ用意する。1つはless_nodesでもう1つはgreater_nodes、最後はsame_nodes
+元のリストから1つずつエレメントを取り出す
+    Xより小さければless_nodes
+    Xと同じならsame_nodes
+    Xより大きければgreater_nodes
+less_nodes+same_nodes+greater_nodesを返す
+
+"""
 import unittest
+import logging
+from test_linked_list import LinkedList, Node
+
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = True
+
+
+class MyLinkedList(LinkedList):
+
+    def partition(self, data):
+        # TODO: Implement me
+        pass
+
+        """Empty list -> []"""
+        """One element list -> [element]"""
+        if len(self.q) < 2:
+            print(f'MyLinkedList.partition: {self.q}')
+            return self.q
+
+        """Left linked list is empty"""
+        """Right linked list is empty"""
+        """General case"""
+        """Partition = 10"""
+        """Input: 4, 3, 7, 8, 10, 1, 10, 12"""
+        """Output: 4, 3, 7, 8, 1, 10, 10, 12"""
+
+        """リストを3つ用意する。1つはless_nodesでもう1つはgreater_nodes、最後はsame_nodes"""
+        less_nodes = []
+        same_nodes = []
+        greater_nodes = []
+        """元のリストから1つずつエレメントを取り出す"""
+        """    Xより小さければless_nodes"""
+        """    Xと同じならsame_nodes"""
+        """    Xより大きければgreater_nodes"""
+
+        for n in self.q:
+            if n.data < data:
+                less_nodes.append(n)
+            if n.data == data:
+                same_nodes.append(n)
+            if n.data > data:
+                greater_nodes.append(n)
+        """less_nodes+same_nodes+greater_nodesを返す"""
+        self.q = less_nodes + same_nodes + greater_nodes
+        return self
 
 
 class TestPartition(unittest.TestCase):
@@ -34,7 +98,7 @@ class TestPartition(unittest.TestCase):
         linked_list.insert_to_front(4)
         partitioned_list = linked_list.partition(10)
         self.assertEqual(partitioned_list.get_all_data(),
-                     [4, 3, 8, 1, 10, 10, 13, 14, 12])
+                         [4, 3, 8, 1, 10, 10, 13, 14, 12])
 
         print('Success: test_partition')
 
