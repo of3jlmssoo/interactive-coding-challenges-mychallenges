c05d8acd7c05bf1ed908b35ba3aedf7c63ee4332
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-25 22:48:44 +0900

    test_math.ops.py completed

--- math_probability/math_ops/test_math_ops.py
+++ math_probability/math_ops/test_math_ops.py
@@ -1,4 +1,49 @@
+"""
+久しぶりにpandasを使ってみる。in O(1)は気にしない。。。
+
+"""
+from typing import Type
 import unittest
+import pandas as pd
+
+
+class Solution(object):
+
+    def __init__(self, upper_limit=100):
+        # TODO: Implement me
+        # pass
+        self.max = None
+        self.min = None
+        self.mean = None
+        self.mode = None
+
+        self.ps = pd.Series([], dtype='float64')
+
+    def insert(self, val):
+        # TODO: Implement me
+        # pass
+        if val is None:
+            raise Type(f'Solution.insert: val == None')
+
+        self.ps = self.ps.append(pd.Series([val]))
+
+        self.update_max()
+        self.update_min()
+        self.update_mean()
+        self.update_mode()
+
+    def update_max(self):
+        self.max = self.ps.max()
+
+    def update_min(self):
+        self.min = self.ps.min()
+
+    def update_mean(self):
+        self.mean = self.ps.mean(axis=0)
+
+    def update_mode(self):
+        s = pd.Series(list(self.ps))
+        self.mode = s.mode()[0]
 
 
 class TestMathOps(unittest.TestCase):
