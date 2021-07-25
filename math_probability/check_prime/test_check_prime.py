bfed29f2c9142cb6d51428b0be21c147d0d77b7a
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-25 23:26:58 +0900

    test_generate_primes.py completed

--- math_probability/check_prime/test_check_prime.py
+++ math_probability/check_prime/test_check_prime.py
@@ -1,4 +1,42 @@
+"""
+[参考]
+https://www.educative.io/courses/introduction-to-computers-and-programming/g2l3zmBPqxG
+"""
 import unittest
+
+
+class Math(object):
+
+    def check_prime(self, num):
+        # TODO: Implement me
+        # pass
+
+        if num is None or not isinstance(num, int):
+            raise TypeError(f'Math.check_prime: {num}')
+
+        if num < 2:
+            return False
+
+        # if num > 1:
+        #     for i in range(2, num // 2):
+        #         if (num % i) == 0:
+        #             print(num, "is not a prime number")
+        #             break
+        #         else:
+        #             print(num, "is a prime number")
+        # else:
+        #     print(num, "is not a prime number")
+
+        i = 2
+        result = True
+        while i <= num / 2:
+            rem = num % i
+            if rem != 0:
+                i = i + 1
+            else:
+                result = False
+                break
+        return result
 
 
 class TestMath(unittest.TestCase):
@@ -10,6 +48,7 @@ class TestMath(unittest.TestCase):
         self.assertEqual(math.check_prime(0), False)
         self.assertEqual(math.check_prime(1), False)
         self.assertEqual(math.check_prime(97), True)
+        self.assertEqual(math.check_prime(4), True)
         print('Success: test_check_prime')
 
 
--- math_probability/generate_primes/test_generate_primes.py
+++ math_probability/generate_primes/test_generate_primes.py
@@ -1,4 +1,24 @@
 import unittest
+from test_check_prime import Math
+
+
+class PrimeGenerator(object):
+
+    def generate_primes(self, max_num):
+        # TODO: Implement me
+        # pass
+
+        result = []
+
+        if max_num is None or not isinstance(max_num, int):
+            raise TypeError(f'Math.check_prime: {max_num}')
+
+        m = Math()
+        for i in range(max_num):
+            # print(i, m.check_prime(i))
+            result = result + [m.check_prime(i)]
+
+        return result
 
 
 class TestMath(unittest.TestCase):
@@ -7,13 +27,30 @@ class TestMath(unittest.TestCase):
         prime_generator = PrimeGenerator()
         self.assertRaises(TypeError, prime_generator.generate_primes, None)
         self.assertRaises(TypeError, prime_generator.generate_primes, 98.6)
-        self.assertEqual(prime_generator.generate_primes(20), [False, False, True, 
-                                                           True, False, True, 
-                                                           False, True, False, 
-                                                           False, False, True, 
-                                                           False, True, False, 
-                                                           False, False, True, 
-                                                           False, True])
+
+        prime_generator.generate_primes(20)
+
+        self.assertEqual(prime_generator.generate_primes(20),
+                         [False,
+                          False,
+                          True,
+                          True,
+                          False,
+                          True,
+                          False,
+                          True,
+                          False,
+                          False,
+                          False,
+                          True,
+                          False,
+                          True,
+                          False,
+                          False,
+                          False,
+                          True,
+                          False,
+                          True])
         print('Success: generate_primes')
 
 
