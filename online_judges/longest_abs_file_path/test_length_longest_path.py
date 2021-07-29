7afd8bd5d99b896cc7a366087e63f67d03d6a65d
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-07-29 11:28:56 +0900

    test_length_longest_path.py comment updated

--- online_judges/longest_abs_file_path/test_length_longest_path.py
+++ online_judges/longest_abs_file_path/test_length_longest_path.py
@@ -7,6 +7,50 @@ dir \n\t subdir1 \n\t\t file1.ext
 3       7               10                  9    = 29
    1            1                   1            = 3
 
+dir subdir1  file1.ext
+dir subdir1  subsubdir1
+dir tsubdir2 subsubdir2 file2.ext
+
+
+
+[データ]
+1カラム目の数値は\tの数
+2カラム目はフォルダーかファイルの名前
+
+0 dir 
+1 subdir1
+2 file1.ext
+2 subsubdir1
+1 subdir2
+2 subsubdir2
+3 file2.ext
+
+[データ保管(リスト)]
+input_list = [[0, dir], [1,subdir1] ...]
+output_list = [
+    [dir, subdir1,  file1.ext],
+    [dir, subdir1, subsubdir1],
+    [dir, tsubdir2, subsubdir2, file2.ext]
+]
+
+[処理]
+output_list_index = 0
+t_num = -1 
+リストを順に処理
+    \tの数チェック(inputlist[x][0]-t_num)
+        +1      名称をoutput_listのoutput_list_indexのリストに追加
+        同じ    (output_listのエレメントをコピーしてコピーされたエレメントの最終エレメント(リスト内リスト)を更新する)
+                output_list.append(output_list[output_list_index])
+                output_list_index += 1
+                output_list[output_list_index][-1] = 名称
+        -1      (output_listのエレメントを範囲指定でコピーして名称をアペンドする)
+                output_list.append(output_list[output_list_index][0:1])  
+                output_list_index += 1
+                output_list[output_list_index].append(名称)
+    t_num更新 (t_num=inputlist[x][0])
+
+
+TODO:以下は削除する
 'dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext' -> 32
 
 dir
@@ -80,7 +124,7 @@ class Solution(object):
         print(m)
 
         for n in m:
-            print(n.count('\t'))
+            print(n.count('\t'), re.sub('\n\t+','',n))
 
 class TestSolution(unittest.TestCase):
 
