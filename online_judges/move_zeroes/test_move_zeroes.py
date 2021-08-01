9fd0fc80d7c89b9678e3dafdc43b3e35bc1d8052
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-08-01 21:31:01 +0900

    test_move_zeroes.py completed

--- online_judges/move_zeroes/test_move_zeroes.py
+++ online_judges/move_zeroes/test_move_zeroes.py
@@ -1,4 +1,38 @@
 import unittest
+import logging
+
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = True
+
+
+class Solution(object):
+
+    def move_zeroes(self, nums):
+        """ Is the output a new array of ints?
+            No, do this in-place
+        """
+        # TODO: Implement me
+        pass
+
+        if nums is None:
+            raise TypeError(f'Solution.move_zeroes: nums is None')
+
+        for n in nums:
+            if n == 0:
+                nums.remove(n)
+                nums.append(n)
+
+        return nums
 
 
 class TestMoveZeroes(unittest.TestCase):
@@ -9,18 +43,23 @@ class TestMoveZeroes(unittest.TestCase):
         array = [0, 1, 0, 3, 12]
         solution.move_zeroes(array)
         self.assertEqual(array, [1, 3, 12, 0, 0])
+
         array = [1, 0]
         solution.move_zeroes(array)
         self.assertEqual(array, [1, 0])
+
         array = [0, 1]
         solution.move_zeroes(array)
         self.assertEqual(array, [1, 0])
+
         array = [0]
         solution.move_zeroes(array)
         self.assertEqual(array, [0])
+
         array = [1]
         solution.move_zeroes(array)
         self.assertEqual(array, [1])
+
         array = [1, 1]
         solution.move_zeroes(array)
         self.assertEqual(array, [1, 1])
