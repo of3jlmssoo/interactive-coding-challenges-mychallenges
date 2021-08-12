238a27e4c5bca208ebc236a2087d2ae213c4d140
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-08-12 22:58:29 +0900

    test_knapsack.py worked but formatting required

--- recursion_dynamic/knapsack_01/test_knapsack.py
+++ recursion_dynamic/knapsack_01/test_knapsack.py
@@ -68,6 +68,14 @@ class Item(object):
         return self.label + ' v:' + str(self.value) + ' w:' + str(self.weight)
 
 
+def checkLogStatus(logmsg, opt=''):
+    if not logger.disabled:
+        if opt == 'end':
+            print(logmsg, end='')
+        else:
+            print(logmsg)
+
+
 class KnapsackTopDown(object):
 
     def fill_knapsack(self, input_items, total_weight):
@@ -127,41 +135,68 @@ class Knapsack(object):
         weights = [i.weight for i in input_items]
         # return self.solveksbu(profits, weights, total_weight)
         result = self.solveksbu(profits, weights, total_weight)
-        print('result ----------------------------', result)
+        checkLogStatus(
+            f'result from self.solveksbu {result}\n{result[-1][-1]}')
+        # return result[-1][-1]
+        return self.findItemfromvalue(input_items, result[-1][-1])
+        # print(result[0].label)
+        # print(result[1].label)
+
+    def findItemfromvalue(self, input_items, lst):
+        result = []
+        for l in lst:
+            for i in input_items:
+                if l == i.value:
+                    result.append(i)
+                    continue
         return result
 
     def solveksbu(self, profits, weights, capacity):
-        print('P:', profits, 'W:', weights, 'C:', capacity)
+        # print('P:', profits, 'W:', weights, 'C:', capacity)
+        checkLogStatus(
+            f'solveksbu() args: P: {profits}, W: {weights}, C: {capacity}')
         if (capacity <= 0 or len(profits) == 0 or len(weights) != len(profits)):
             return 0
-        print('P:', profits, 'W:', weights, 'C:', capacity)
+        # print('P:', profits, 'W:', weights, 'C:', capacity)
 
-        n = len(profits)
+        # n = len(profits)
         # dp = [[None] * (capacity + 1)] * len(profits)
         W = [[None for j in range(capacity + 1)]
              for i in range(len(profits) + 1)]
-        X = [[[None] for j in range(capacity + 1)]
+        X = [[[] for j in range(capacity + 1)]
              for i in range(len(profits) + 1)]
-        print('W(0) ', W)
-        print('X(0) ', X)
+        # print('W(0) ', W)
+        # print('X(0) ', X)
+        checkLogStatus(f'W(0) initialized:{W}\n')
+        checkLogStatus(f'X(0) initialized:{X}\n')
 
         for w in range(capacity + 1):
             W[0][w] = 0
-        print('W(1) ', W)
+        # print('W(1) ', W)
+        checkLogStatus(f'W[0][x] updated :{W}\n')
 
         for i in range(1, len(profits) + 1):
             for w in range(capacity + 1):
-                print(f'i:{i}, w:{w} --- ', end='')
+                # print(f'i:{i}, w:{w} --- ', end='')
+                checkLogStatus(
+                    f'second loop started. i:{i}, w:{w} --- ', 'end')
                 if weights[i - 1] > w:
-                    print(
-                        f'then: weights[i-1]:{weights[i-1]} > w:{w} --- ',
-                        end='')
-                    print(f'W[{i}][{w}] = W[{i-1}][{w}]')
+                    # print(
+                    #     f'then: weights[i-1]:{weights[i-1]} > w:{w} --- ',
+                    #     end='')
+                    checkLogStatus(
+                        f'then: weights[i-1]:{weights[i-1]} > w:{w} --- ', 'end')
+                    # print(f'W[{i}][{w}] = W[{i-1}][{w}]')
+                    checkLogStatus(f'W[{i}][{w}] = W[{i-1}][{w}]')
                     W[i][w] = W[i - 1][w]
-                    print(X[i][w], type(X[i][w]))
                     X[i][w].append(W[i - 1][w])
                 else:
-                    print(
+                    # print(
+                    # f'else:                          W[{i}][{w}] =
+                    # max(W[{i-1}][{w}],
+                    # profits[{i-1}]+W[{i-1}][{w}-weights[{i-1}]]) --- max(
+                    # {W[i-1][w]}, {profits[i-1]}+{W[i-1][w-weights[i-1]]} ) ')
+                    checkLogStatus(
                         f'else:                          W[{i}][{w}] = max(W[{i-1}][{w}], profits[{i-1}]+W[{i-1}][{w}-weights[{i-1}]]) --- max( {W[i-1][w]}, {profits[i-1]}+{W[i-1][w-weights[i-1]]} ) ')
                     W[i][w] = max(W[i - 1][w], profits[i - 1] +
                                   W[i - 1][w - weights[i - 1]])
@@ -172,8 +207,11 @@ class Knapsack(object):
                         X[i][w].append(profits[i - 1])
                         X[i][w].append(W[i - 1][w - weights[i - 1]])
 
-        print('W(2) ', W)
-        print(f'{len(profits)}, {capacity} {W[len(profits)][capacity]} ')
+        # print('W(2) ', W)
+        # print(f'{len(profits)}, {capacity} {W[len(profits)][capacity]} ')
+        checkLogStatus(f'W(1)             {W}')
+        checkLogStatus(
+            f'before return len(profits:{len(profits)}, capacity:{capacity} W[len(profits)][capacity]:{W[len(profits)][capacity]}\n')
         # return W[len(profits)][capacity]
         return X
 
@@ -233,12 +271,12 @@ class TestKnapsack(unittest.TestCase):
         total_weight = 8
         expected_value = 13
         results = knapsack.fill_knapsack(items, total_weight)
-        # self.assertEqual(results[0].label, 'd')
-        # self.assertEqual(results[1].label, 'b')
-        # total_value = 0
-        # for item in results:
-        #     total_value += item.value
-        # self.assertEqual(total_value, expected_value)
+        self.assertEqual(results[0].label, 'd')
+        self.assertEqual(results[1].label, 'b')
+        total_value = 0
+        for item in results:
+            total_value += item.value
+        self.assertEqual(total_value, expected_value)
         print('Success: test_knapsack_bottom_up')
 
     def test_knapsack_top_down(self):
