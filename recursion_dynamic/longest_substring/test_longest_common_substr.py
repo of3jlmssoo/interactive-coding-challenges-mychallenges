a0e730f8a62fe9119611406137c86b3b32ecad87
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-08-16 23:24:08 +0900

    test_longest_common_substr.py completed

--- recursion_dynamic/longest_substring/test_longest_common_substr.py
+++ recursion_dynamic/longest_substring/test_longest_common_substr.py
@@ -1,11 +1,30 @@
+"""
+test_longest_common_subseq.pyと同じ内容。
+
+"""
 import unittest
+from test_longest_common_subseq import StringCompare
+
+
+# class StringCompare(object):
+class StringCompare(StringCompare):
+
+    def longest_common_substr(self, str0, str1):
+        # TODO: Implement me
+        # pass
+        print(str0, str1)
+        return super().longest_common_subseq(str0, str1)
 
 
 class TestLongestCommonSubstr(unittest.TestCase):
 
     def test_longest_common_substr(self):
         str_comp = StringCompare()
-        self.assertRaises(TypeError, str_comp.longest_common_substr, None, None)
+        self.assertRaises(
+            TypeError,
+            str_comp.longest_common_substr,
+            None,
+            None)
         self.assertEqual(str_comp.longest_common_substr('', ''), '')
         str0 = 'ABCDEFGHIJ'
         str1 = 'FOOBCDBCDE'
