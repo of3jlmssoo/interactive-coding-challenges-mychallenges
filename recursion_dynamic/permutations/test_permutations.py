4de1ed08539ae6a7112ac6cfc8e4482f2ffd124c
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-08-26 23:14:37 +0900

    test_permutations.py completed

--- recursion_dynamic/permutations/test_permutations.py
+++ recursion_dynamic/permutations/test_permutations.py
@@ -1,4 +1,58 @@
+"""
+- n_pairs_parenthesesを参考にする
+- find_permutations()でリターンする配列の順序については考慮していないがexpectedと同じになったので良しとする
+"""
 import unittest
+import copy
+import logging
+
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+# DEBUG INFO WARNIG ERROR CRTICAL
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+logger.disabled = True
+
+
+class Permutations(object):
+
+    def find_permutations(self, string):
+        # TODO: Implement me
+        # pass
+
+        argchecks = [string is None, string == '']
+
+        if any(argchecks):
+            return string
+
+        n = len(string)
+        work_str = []
+        lst = []
+        _make_permutations(list(string), n, work_str, '', lst)
+        logger.debug(f'lst: {lst}')
+        return lst
+
+
+def _make_permutations(str, n, work_str, sofar, lst):
+    logger.debug(f'_make_permutations called str:{str}, \
+         n:{n}, work_str:{work_str}, sofar:{sofar}, lst:{lst}')
+
+    if n == 0:
+        if sofar not in lst:
+            lst.append(sofar)
+        return
+
+    else:
+        for s in range(n):
+            work_str = copy.deepcopy(str)
+            work_str.remove(str[s])
+            _make_permutations(work_str, n - 1, work_str, sofar + str[s], lst)
 
 
 class TestPermutations(unittest.TestCase):
