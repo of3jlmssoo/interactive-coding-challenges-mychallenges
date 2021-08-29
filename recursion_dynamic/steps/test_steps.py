86d2db40e57cd3751212b4954eee492fdf2606d6
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-08-29 21:56:59 +0900

    test_steps.py progiressing

--- recursion_dynamic/steps/test_steps.py
+++ recursion_dynamic/steps/test_steps.py
@@ -1,6 +1,34 @@
 import unittest
 
 
+class Steps(object):
+
+    def count_ways(self, num_steps):
+        # TODO: Implement me
+        # pass
+
+        chkargs = [num_steps is None, num_steps == -1]
+        if any(chkargs):
+            raise TypeError(f'Step.count_ways: {num_steps=}')
+
+        _steps = [1, 2, 3]
+        _total = 0
+        return self._count_ways(_steps, num_steps, _total)
+
+    def _count_ways(self, _steps, num_steps, _total):
+
+        if _total > num_steps:
+            return 0
+        if _total == num_steps:
+            return 1
+
+        result = 0
+        for s in _steps:
+            result += self._count_ways(_steps, num_steps, _total + s)
+
+        return result
+
+
 class TestSteps(unittest.TestCase):
 
     def test_steps(self):
