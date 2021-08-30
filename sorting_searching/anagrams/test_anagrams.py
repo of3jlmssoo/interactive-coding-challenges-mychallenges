a606a130c9498f95b575263d075f5c36065ade95
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-08-30 23:07:22 +0900

    test_anagrams.py numpy version completed

--- sorting_searching/anagrams/test_anagrams.py
+++ sorting_searching/anagrams/test_anagrams.py
@@ -1,4 +1,49 @@
+"""
+[test_anagrams.py]
+- dataの各エレメントをソート
+- data自体をnumpyでargsort
+- dataにargsortで得られたインデックスを適用
+
+また、resultとexpectedの順番問題あり
+
+[test_anagrams2.py]
+https://github.com/donnemartin/interactive-coding-challenges/blob/master/sorting_searching/anagrams/anagrams_challenge.ipynb
+には、from collections import OrderedDictがあるのでOrderedDictを使えということなのだろう。
+
+"""
 import unittest
+import numpy as np
+import copy
+
+from collections import OrderedDict
+
+
+class Anagram(object):
+
+    def group_anagrams(self, items):
+        # TODO: Implement me
+        # pass
+
+        if items is None:
+            raise TypeError(f'Anagram.group_anagrams: arg error {items=}')
+
+        data_np = np.array(items)
+        element_sorted = [''.join(sorted(s)) for s in data_np]
+        idx = np.argsort(element_sorted)
+        return self.make_sequence(items, data_np[idx])
+
+    def make_sequence(self, items, data_np):
+        data_np_copy = []
+        already_processed = []
+
+        for i in items:
+            if i not in already_processed:
+                idx = list(data_np).index(i)
+                data_np_copy.append(data_np[idx])
+                data_np_copy.append(data_np[idx + 1])
+                already_processed.append(data_np[idx])
+                already_processed.append(data_np[idx + 1])
+        return data_np_copy
 
 
 class TestAnagrams(unittest.TestCase):
