0877897d120e5a4002704b5299854d07d834c873
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-09-03 18:34:50 +0900

    test_insertion_sort.py completed

--- sorting_searching/insertion_sort/test_insertion_sort.py
+++ sorting_searching/insertion_sort/test_insertion_sort.py
@@ -1,4 +1,56 @@
+"""
+[参照]
+https://en.wikipedia.org/wiki/Insertion_sort のアニメ
+
+result_list : ソート済みのリスト
+tmp_list :　その際のデータとresult_listを比較した結果を保管
+
+dataのデータを順に処理
+データとresult_listを比較し大きい場合1、小さい場合0
+1が無い場合result_istにappend
+1がある場合tmp_listの最初の1の位置をとって、result_listのその位置に追加
+
+
+"""
 import unittest
+from collections import deque
+
+
+class InsertionSort(object):
+
+    def __init__(self) -> None:
+        # self.d = deque()
+        self.result_list = []
+
+    def sort(self, data):
+        # TODO: Implement me
+        # pass
+        if data is None:
+            raise TypeError(f'InsertionSort.sort: arg error {data=}')
+
+        if len(data) <= 1:
+            return data
+
+        # print(f'{data=}')
+        # self.queue_append(data)
+
+        # for q in self.d:
+        for q in data:
+            # tmp_list = [1 if q < d else 0 for d in self.result_list]
+            # if sum(tmp_list) == 0:
+            if sum(tmp_list := [1 if q < d else 0 for d
+                                in self.result_list]) == 0:
+                self.result_list.append(q)
+            else:
+                # idx = tmp_list.index(1)
+                self.result_list.insert(tmp_list.index(1), q)
+            # print(f'{q=} {self.result_list=} \n')
+
+        return self.result_list
+
+    def queue_append(self, data):
+        for i in data:
+            self.d.append(i)
 
 
 class TestInsertionSort(unittest.TestCase):
