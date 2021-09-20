86155001affc8c9c170eae0379fad5c1ac5e9e1b
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-09-20 21:49:17 +0900

    test_queue_from_stacks.py completed

--- stacks_queues/queue_from_stacks/test_queue_from_stacks.py
+++ stacks_queues/queue_from_stacks/test_queue_from_stacks.py
@@ -1,4 +1,76 @@
+"""
+[参照]
+https://stackoverflow.com/questions/69192/how-to-implement-a-queue-using-two-stacks
+
+enqueue(0)
+            0
+            stackA
+enqueue(1)
+            1
+            0
+            stackA
+enqueue(2)
+            2
+            1
+            0
+            stackA
+dequeue()
+                        2
+                    1
+                    0
+            stackA  stackB
+dequeue()
+                        1
+                    0
+            stackA  stackB
+dequeue()
+                        0
+            stackA  stackB
+enqueue(5)
+            5
+            stackA
+dequeue()
+                    5
+            stackA  stackB
+"""
 import unittest
+from test_stack import Stack
+from test_linked_list import Node
+
+
+class QueueFromStacks(object):
+
+    def __init__(self):
+        # TODO: Implement me
+        # pass
+        self.stackA = Stack(None)
+        self.stackB = Stack(None)
+
+    def shift_stacks(self, source, destination):
+        # TODO: Implement me
+        # pass
+        while source.peek() is not None:
+            destination.push(source.pop())
+
+    def enqueue(self, data):
+        # TODO: Implement me
+        # pass
+        self.shift_stacks(self.stackB, self.stackA)
+        self.stackA.push(data)
+
+    def dequeue(self):
+        # TODO: Implement me
+        # pass
+        # print(f'{self.stackB.pop()=}')
+        self.shift_stacks(self.stackA, self.stackB)
+        return self.stackB.pop()
+
+    def list_queues(self):
+        print(f'QueueFromStacks.list_queues: called. stackA')
+        self.stackA.list_nodes()
+        print('\nstackB')
+        self.stackB.list_nodes()
+        print('\n')
 
 
 class TestQueueFromStacks(unittest.TestCase):
@@ -14,6 +86,8 @@ class TestQueueFromStacks(unittest.TestCase):
         num_items = 3
         for i in range(0, num_items):
             queue.enqueue(i)
+
+        # queue.list_queues()
 
         print('Test: Dequeue on non-empty stack')
         print('Test: Dequeue after an enqueue')
